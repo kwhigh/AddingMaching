3ab35d2bb732cae4a64994111ec86479ca656ea6
khigh <khigh@KeenansWhitePC>
2020-10-07 19:19:07 -0700

    Final version of AddingMachine

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,36 +1,50 @@
 package cse360assignment02;
 
+import java.util.*;
+
 public class AddingMachine {
 	 private int total;
+	 private List<String> listOfValues = new ArrayList<String>();
+	 private String firstString = "";
+	 private String finalString;
+	
+	 public AddingMachine () 
+	 {
+		 total = 0;  // not needed - included for clarity
+	 }
 	  
-	  public AddingMachine () 
-	  {
-	    total = 0;  // not needed - included for clarity
-	  }
+	 public int getTotal () //gets the total and returns it
+	 {
+		 return total;
+	 }
 	  
-	  public int getTotal () 
-	  {
-	    return 0;
-	  }
-	  
-	  public void add (int value) 
-	  {
-		  
-	  }
+	 public void add (int value) //adds the value passed to the total
+	 {
+		 total = total + value;
+		 listOfValues.add(" + " + value);
+	 }
 
-	  public void subtract (int value) 
-	  {
-		  
-	  }
+	 public void subtract (int value) //subtracts the value passed from the total
+	 {
+		 total = total - value;
+		 listOfValues.add(" - " + value);
+	 }
 
-	  public String toString () 
-	  {
-	    return "";
-	  }
-
-	  public void clear() 
-	  {
+	 public String toString () // creates a string of the history of the values added to the adding machine
+	 {
+		 for (int i = 0; i < listOfValues.size(); i++)
+		 {
+			 firstString = firstString + listOfValues.get(i);
+			 finalString = "0" + firstString;
+		 }
 		  
-	  }
+		 return finalString;
+	 }
+
+	 public void clear() 
+	 {
+		 total = 0;
+		 listOfValues.clear();
+	 }
 	  
 }
